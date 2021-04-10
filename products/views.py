22e79506ab8defffff72a67a32244518f2622a3a
Samuel Denton <samdenton1995@hotmail.co.uk>
2021-04-10 18:26:25 +0000

    created a products template and the view/model and url for this

--- products/views.py
+++ products/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.db.models.functions import Lower
+
+from .models import Product, Category
+
 
 # Create your views here.
+
+
+def all_products(request):
+    """ A view to show all products, includes sorting and search queries """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+
+    }
+
+    return render(request, 'products/products.html', context)
