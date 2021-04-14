cda025df8e07f67a5192fce2128cb85d683ba871
Samuel Denton <samdenton1995@hotmail.co.uk>
2021-04-14 14:13:25 +0000

    used code from boutique ado to set out basic shell for the bag app, will customise at a later date

--- products/views.py
+++ products/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
+from django.db.models.functions import Lower
+
 from .models import Product, Category
+from .forms import ProductForm
 
 # Create your views here.
 
 
 def all_products(request):
-    """ A view to show all products, including sorting and search queries """
+    """ A view to show all products, includes sorting and search queries """
 
     products = Product.objects.all()
     query = None
@@ -16,12 +20,12 @@ def all_products(request):
     direction = None
 
     if request.GET:
-
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
             sort = sortkey
+
             if sortkey == 'name':
-                sortkey == 'lower_name'
+                sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
 
             if sortkey == 'category':
@@ -40,9 +44,10 @@ def all_products(request):
 
         if 'q' in request.GET:
             query = request.GET['q']
+
             if not query:
                 messages.error(
-                    request, "You didn't enter any search criteria!")
+                    request, "You did not enter anys earch criteria")
                 return redirect(reverse('products'))
 
             queries = Q(name__icontains=query) | Q(
@@ -71,3 +76,69 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+@login_required
+def add_product(request):
+    """Add product to the store"""
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that!')
+        return redirect(reverse('home'))
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save()
+            messages.success(request, 'Successfully added product')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(
+                request, 'Failed to add product. Please ensure the form is valid')
+    else:
+        form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form
+    }
+    return render(request, template, context)
+
+
+@login_required
+def edit_product(request, product_id):
+    """Edit a product in the store"""
+    if not request.user.is_superuser:
+        messages.errro(request, 'Sorry, only store owners can do that!')
+        return redirect(reverse('home'))
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request, f'You have succesfully updated {product.name}')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(
+                request, f'Failed to edit {product.name}, please ensure the form is valid')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product
+    }
+    return render(request, template, context)
+
+
+@login_required
+def delete_product(request, product_id):
+    """Delete a product form the store"""
+    if not request.user.is_superuser:
+        messages.errro(request, 'Sorry, only store owners can do that!')
+        return redirect(reverse('home'))
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(
+        request, f'Successfully deleted {product.name} from the store!')
+    return redirect(reverse('products'))
