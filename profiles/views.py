b21184a0749eddf14ba45281087be67bed45f650
Samuel Denton <samdenton1995@hotmail.co.uk>
2021-04-21 10:58:01 +0000

    product admin is up and running, need to style these to distance myself from the mini project

--- profiles/views.py
+++ profiles/views.py
@@ -15,8 +15,10 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-
-    form = UserProfileForm(instance=profile)
+        else:
+            messages.error(request, 'Failed to update information, please check the form and try again')
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
