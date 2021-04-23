5ddfc4631c4d4ab3f2dce7f28c1764e02282f5ca
Samuel Denton <samdenton1995@hotmail.co.uk>
2021-04-23 16:54:25 +0000

    added in local host into allowed hosts

--- toki/settings.py
+++ toki/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['toki-damsenton.herokuapp.com', 'locahost']
+ALLOWED_HOSTS = ['toki-damsenton.herokuapp.com', 'localhost']
 
 
 # Application definition
