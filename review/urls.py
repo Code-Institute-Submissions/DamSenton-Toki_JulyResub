24c08f1f5123de93a52c51568a7cb5a601c32a73
Samuel Denton <samdenton1995@hotmail.co.uk>
2021-04-25 18:17:21 +0000

    removed previous review app as it wasn't working how i wanted it to, going to start from scratch

--- review/urls.py
+++ review/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('new_review/<product_id>', views.new_review, name='new_review'),
+    path('', views.new_review, name='new_review'),
 ]
--- review/views.py
+++ review/views.py
@@ -1,32 +1,19 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
-from django.contrib import messages
+from django.shortcuts import render, redirect, get_object_or_404
 from products.models import Product
-from .forms import Product_review_form
 
 
-def review_form_page(request, product_id):
-    """ A view to take a user to the review form """
-    return render(request, 'review/review-form.html')
+# Create your views here.
 
 
 def new_review(request, product_id):
     """
-    Handles the POST request for review form. Saves the form and redirects
-    to the product selected page
+    A view to take users to a review page where
+    they can leave a message and a rating for a product
     """
     product = get_object_or_404(Product, pk=product_id)
 
-    if request.method == 'POST':
-        review_form = Product_review_form(request.POST)
-        if review_form.is_valid():
-            review = review_form.save(commit=False)
-            review.product = product
-            review.user = request.user
-            review.save()
-            messages.info(
-                request, "Thank you for the review!")
-            return redirect(reverse('product_detail', args=[product_id]))
-        else:
-            print(review_form.errors)
+    context = {
+        'product': product,
+    }
 
-    return redirect(reverse('product_detail', args=[product_id]))
+    return render(request, 'review/review_form.html', context)
