623d822eb2e768eac2ca02f1f67ca7c041dd13d9
Samuel Denton <samdenton1995@hotmail.co.uk>
2021-04-27 20:13:03 +0000

    dumped review and articles data into new .json files to see if this fixes my heroku issue

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Articles
 
 # Register your models here.
+
+
+class ArticlesAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'pk',
+        'date',
+        'slug'
+    )
+
+
+admin.site.register(Articles, ArticlesAdmin)
